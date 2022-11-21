9e61db54dd4e030833f6d04c95515a46ae6ac8d9
Danielle Souza <dany.sa1@hotmail.com>
2022-11-21 08:41:31 -0300

    feat: using styled components

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Welcome from './src/components/Welcome';
 import DashBoard from './src/screens/Dashboard';
+import {
+  useFonts,
+  Poppins_400Regular, 
+  Poppins_500Medium, 
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
+import { ThemeProvider } from 'styled-components';
+import theme from './src/global/styles/theme';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
-  return <DashBoard />
+  const [ fontsLoaded ] = useFonts({
+    Poppins_400Regular, 
+    Poppins_500Medium, 
+    Poppins_700Bold
+  });
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <DashBoard />
+    </ThemeProvider>
+  )
 }
 
 
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,13 +1,14 @@
-import { View } from 'react-native';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
-    background-color: #999;
     align-items: center;
     justify-content: center;
+    background-color: ${({theme})=> theme.colors.background};
 `;
 
 export const Title = styled.Text`
     font-size: 24px;
+    color: ${({theme})=> theme.colors.title};
+    font-family:${({theme})=> theme.fonts.bold}; 
 `;
